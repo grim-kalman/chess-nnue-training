bd72da9a653e1978952505e5df27093a4edd799d
Grim Kalman <grim.e.kalman@gmail.com>
2024-07-20 14:24:25 +0200

    feat: refactors the fetch_user_games.py script

--- fetch_user_games.py
+++ fetch_user_games.py
@@ -1,17 +1,18 @@
 import os
 import requests
 
+# Define headers once
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+def fetch_data(url):
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
+
 def get_user_archives(username):
     url = f"https://api.chess.com/pub/player/{username}/games/archives"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
-    
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        return []
-
-    data = response.json()
+    data = fetch_data(url)
     return data.get("archives", [])
 
 def get_user_games(archives):
@@ -19,19 +20,14 @@ def get_user_games(archives):
         os.makedirs("games")
     
     for archive in archives:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
-        response = requests.get(archive, headers=headers)
-        if response.status_code != 200:
-            continue
-
-        data = response.json()
+        data = fetch_data(archive)
         games = data.get("games", [])
         for game in games:
             file_path = os.path.join("games", f"{game['uuid']}.txt")
             with open(file_path, 'w') as file:
-                file.write(str(game.get("pgn", "")))
+                file.write(game.get("pgn", ""))
 
-archives = get_user_archives("GrimKalman")
-get_user_games(archives)
+if __name__ == "__main__":
+    username = "GrimKalman"
+    archives = get_user_archives(username)
+    get_user_games(archives)
